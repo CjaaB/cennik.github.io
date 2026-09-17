5e48f1caed4cdc92dceae03e6cbb81387a1803b5
CjaaB <yakasu1999@gmail.com>
2026-09-17 21:43:55 +0200

    Delete site directory

--- site/js/cad-doc-print.js
+++
@@ -1,9 +0,0 @@
-/**
- * Kompatybilność wsteczna — logika w cad-print.js
- */
-(function () {
-    "use strict";
-    if (!window.CAD_printDocument) {
-        console.warn("[CAD] Załaduj site/js/cad-print.js przed cad-doc-print.js");
-    }
-})();
